86dbab96542c39e4e0e03365675fa8a4c8308c33
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-08 09:48:34 -0800

    Updated 'base_mode.py'
    Updated the __init__ constructor so that if a dictionary instance of the class is entered it will set the attributes of the current class to it. Else initiat the id, created_at & updated_at attributes

--- models/base_model.py
+++ models/base_model.py
@@ -23,14 +23,23 @@ class BaseModel:
     created_at = None
     updated_at = None
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """__init__
 
         The BaseModel constructor.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                # If the key is the time change it from string to datetime
+                if key in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+
+                if key != '__class__':
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """__str__

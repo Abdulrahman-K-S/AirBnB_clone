be65ee5614570ce246d974107ff4231aefab929f
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-08 11:19:03 -0800

    Updated 'base_mode.py'
    Tweaked the __init__ & save methods to update the storage

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ Contains the class BaseModel and it's functions & attributes
 """
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -40,6 +41,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """__str__
@@ -57,6 +59,7 @@ class BaseModel:
         with the current datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """to_dict

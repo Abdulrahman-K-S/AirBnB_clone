4864ef037244fec32107bc5cd6304cad99ec5204
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-12-09 09:27:49 -0800

    Finalized 'console.py'

--- console.py
+++ console.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""console.py
-
-The file that contains the functionality for the console
-of the hbnb project.
+"""Console Module
+This module controls all databases.
+Can create, modify and delete instances.
 """
+
+
+from datetime import datetime
 import cmd
 import models
-import shlex
-from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import re
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
-    """HBNBCommand
-
-    A class that contains all the functionalities for the
-    console of the AirBnB project.
-    """
-    prompt = "(hbnb) "
-    allowed_classes = ['BaseModel', 'User']
+    """command processor class."""
+    prompt = '(hbnb) '
+    allowed_classes = ['BaseModel', 'User', 'State', 'City',
+                       'Amenity', 'Place', 'Review']
 
     def do_quit(self, line):
-        """Quit command to quit the program.
+        """Quit command to exit the program.
         """
         return True
 
     def do_EOF(self, line):
-        """Quit command to quit the program.
+        """Quit command to exit the program.
         """
         return True
 
@@ -93,9 +97,7 @@ based or not on the class name.
             print([str(objs[obj]) for obj in objs])
         elif class_name in self.allowed_classes:
             keys = objs.keys()
-            print([str(objs[key]) for key in keys if key.startswith(
-                class_name
-            )])
+            print([str(objs[key]) for key in keys if key.startswith(class_name)])
         else:
             print("** class doesn't exist **")
 
@@ -126,10 +128,83 @@ by adding or updating attribute.
                 setattr(inst_data, 'updated_at', datetime.now())
                 models.storage.save()
 
-    def emptyline(self):
-        """emptyline
+    def analyze_parameter_value(self, value):
+        """Checks a parameter value for an update
 
-        Do nothing when the promt is an emptyline.
+        Analyze if a parameter is a string that needs
+        convert to a float number or an integer number.
+
+        Args:
+            value: The value to analyze
+
+        """
+        if value.isdigit():
+            return int(value)
+        elif value.replace('.', '', 1).isdigit():
+            return float(value)
+
+        return value
+
+    def get_objects(self, instance=''):
+        """Gets the elements created by the console
+
+        This method takes care of obtaining the information
+        of all the instances created in the file `objects.json`
+        that is used as the storage engine.
+
+        When an instance is sent as an argument, the function
+        takes care of getting only the instances that match the argument.
+
+        Args:
+            instance (:obj:`str`, optional): The instance to finds into
+                the objects.
+
+        Returns:
+            list: If the `instance` argument is not empty, it will search
+            only for objects that match the instance. Otherwise, it will show
+            all instances in the file where all objects are stored.
+
+        """
+        objects = models.storage.all()
+
+        if instance:
+            keys = objects.keys()
+            return [str(val) for key, val in objects.items()
+                    if key.startswith(instance)]
+
+        return [str(val) for key, val in objects.items()]
+
+    def default(self, line):
+        """
+        When the command prefix is not recognized, this method
+        looks for whether the command entered has the syntax:
+            "<class name>.<method name>" or not,
+        and links it to the corresponding method in case the
+        class exists and the method belongs to the class.
+
+        """
+        if '.' in line:
+            splitted = re.split(r'\.|\(|\)', line)
+            class_name = splitted[0]
+            method_name = splitted[1]
+
+            if class_name in self.allowed_classes:
+                if method_name == 'all':
+                    print(self.get_objects(class_name))
+                elif method_name == 'count':
+                    print(len(self.get_objects(class_name)))
+                elif method_name == 'show':
+                    class_id = splitted[2][1:-1]
+                    self.do_show(class_name + ' ' + class_id)
+                elif method_name == 'destroy':
+                    class_id = splitted[2][1:-1]
+                    self.do_destroy(class_name + ' ' + class_id)
+
+    def emptyline(self):
+        """
+        When an empty line is entered in response to the prompt,
+        it won't repeat the last nonempty command entered.
+
         """
         pass
 
